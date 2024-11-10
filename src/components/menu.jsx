17741fb6c85f63b512c8ae3342d4be0597d6d0fa
Carlos Alberto Lozano Morales <rockcarlitos90@gmail.com>
2024-11-10 10:40:19 -0600

    .

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -6,7 +6,7 @@ function Menu({ loggedInUser, onLogout }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Inicio</Link>
+        <img src={logo} alt="" width={50} height={50} />
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
